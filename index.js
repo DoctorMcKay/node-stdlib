9a95ae1382e7b6078c41bfca68920f117223a03b
Alex Corn <mckay@doctormckay.com>
2018-01-14 23:41:43 -0500

    Added orderedArgs

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ StdLib.Concurrency = require('./components/concurrency.js');
 StdLib.DataStructures = require('./components/data_structures.js');
 StdLib.HTTP = require('./components/http.js');
 StdLib.IPv4 = require('./components/ipv4.js');
+StdLib.Parsing = require('./components/parsing.js');
 StdLib.Promises = require('./components/promises.js');

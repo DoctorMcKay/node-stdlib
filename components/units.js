4ca8e241093ea74422e0fc4908fce6500691b513
Alex Corn <alex@alexcorn.com>
2021-02-05 20:23:04 -0500

    Added forceDecimal argument to humanReadableBytes

--- components/units.js
+++ components/units.js
@@ -4,9 +4,10 @@ const Units = module.exports;
  * Return a string containing the human-readable representation of the input byte count.
  * @param {int} bytes
  * @param {boolean} [binary=false] - Pass true to use the binary system instead of the decimal system (i.e. MiB instead of MB)
+ * @param {boolean} [forceDecimal=false] - Pass true to always append the tenths decimal place, even if it's 0
  * @return {string}
  */
-Units.humanReadableBytes = function(bytes, binary) {
+Units.humanReadableBytes = function(bytes, binary = false, forceDecimal = false) {
 	let units = ['K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
 	let suffix = binary ? 'iB' : 'B';
 	let base = binary ? 1024 : 1000;
@@ -20,7 +21,7 @@ Units.humanReadableBytes = function(bytes, binary) {
 		// this is the unit we want if it's the last, or dividing by the next highest is < 1
 		if (i == units.length - 1 || bytes / Math.pow(base, i + 2) < 1) {
 			let bytesVal = bytes / Math.pow(base, i + 1);
-			return (Math.round(bytesVal * 10) / 10) + ' ' + units[i] + suffix;
+			return (forceDecimal ? bytesVal.toFixed(1) : (Math.round(bytesVal * 10) / 10)) + ' ' + units[i] + suffix;
 		}
 	}
 };
--- test/units.js
+++ test/units.js
@@ -4,6 +4,7 @@ const Units = StdLib.Units;
 checkStringEqual("humanReadableBytes(7894561)", "7.9 MB", Units.humanReadableBytes(7894561));
 checkStringEqual("humanReadableBytes(7894561, true)", "7.5 MiB", Units.humanReadableBytes(7894561, true));
 checkStringEqual("humanReadableBytes(1000000000)", "1 GB", Units.humanReadableBytes(1000000000));
+checkStringEqual("humanReadableBytes(1000000000, false, true)", "1.0 GB", Units.humanReadableBytes(1000000000, false, true));
 
 function checkStringEqual(testName, expected, actual) {
 	if (expected !== actual) {

1303dd359408cfe786e5cb3d6ab33631a199f7f4
Alex Corn <alex@alexcorn.com>
2023-09-13 00:08:21 -0400

    Fixed error if no HOME or APPDATA is defined on respective platforms
    (fixes #3)

--- components/os.js
+++ components/os.js
@@ -18,17 +18,31 @@ OS.appDataDirectory = function(params) {
 
 	switch (params.platform || process.platform) {
 		case 'darwin':
-			return Path.join(process.env.HOME, 'Library', 'Application Support', params.appName);
+			if (process.env.HOME) {
+				return Path.join(process.env.HOME, 'Library', 'Application Support', params.appName);
+			}
+
+			// No HOME env var
+			return null;
 
 		case 'win32':
 			let appDataVar = params.useRoaming ? 'APPDATA' : 'LOCALAPPDATA';
-			return Path.join(process.env[appDataVar] || process.env.APPDATA, params.appAuthor, params.appName);
+			let basePath = process.env[appDataVar] || process.env.APPDATA;
+			if (basePath) {
+				return Path.join(basePath, params.appAuthor, params.appName);
+			}
+
+			// No APPDATA or LOCALAPPDATA env var
+			return null;
 
 		default:
 			if (process.env.XDG_DATA_HOME) {
 				return Path.join(process.env.XDG_DATA_HOME, params.appName);
-			} else {
+			} else if (process.env.HOME) {
 				return Path.join(process.env.HOME, '.local', 'share', params.appName);
+			} else {
+				// No XDG_DATA_HOME or HOME env var
+				return null;
 			}
 	}
 };

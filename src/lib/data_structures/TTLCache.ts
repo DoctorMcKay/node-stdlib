0fe2fda4f8729eb3ec41a02dbc7df08a09e9aa6a
Sadzurami <sadzurami2036@gmail.com>
2023-10-01 14:36:49 +0300

    add ttlcache

---
+++ src/lib/data_structures/TTLCache.ts
@@ -0,0 +1,64 @@
+export default class TTLCache {
+  private readonly container = new Map<string, any>();
+	private readonly timeouts = new Map<string, any>();
+
+  /**
+   * Construct a new TTLCache.
+   * @param {int} ttl Time to live in milliseconds for each entry
+   * @constructor
+   */
+	constructor(private readonly ttl?:number) {}
+
+  /**
+	 * Add an entry to the cache.
+	 * @param {string} key - The key under which this entry should be stored
+	 * @param {*} val - The value to store in this entry
+	 */
+	add(key:string, val:any, ttl?:number) {
+		this.container.set(key, val);
+
+		ttl = ttl || this.ttl;
+		if (ttl) this.timeouts.set(key, setTimeout(() => this.delete(key), ttl).unref());
+	}
+
+	/**
+	 * Get the entry stored in the cache under a particular key.
+	 * @param {string} key - The key to retrieve
+	 * @return {null|*} value if present, null if not
+	 */
+	get(key:string) {
+		const value = this.container.get(key);
+    return typeof value !== 'undefined' ? value : null;
+	}
+
+  /**
+	 * Delete an entry from the cache.
+	 * @param {string} key
+   * @returns {void}
+	 */
+	delete(key:string) {
+    this.container.delete(key);
+
+		if (this.timeouts.has(key)) {
+			clearTimeout(this.timeouts.get(key));
+			this.timeouts.delete(key);
+		}
+	}
+
+  /**
+	 * Get a list of all keys in the cache.
+	 * @returns {string[]}
+	 */
+  getKeys() {
+    return [...this.container.keys()];
+  }
+
+  /**
+   * Clear the cache.
+   * @returns {void}
+   */
+	clear() {
+		this.container.clear();
+    this.timeouts.forEach(timeout => clearTimeout(timeout));
+	}
+}
